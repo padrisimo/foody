288ab862b5a9b6d39c65d5b8dc68b5fb2d0e0827
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-21 20:53:46 +0100

    build the splash screen

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
 import React, { Component } from 'react';
 import AppNavigator from './app/app.navigator';
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
       <AppNavigator />
--- app/app.navigator.js
+++ app/app.navigator.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { DrawerNavigator } from 'react-navigation';
+import SplashScreen from './screens/splash.screen';
 
-const RouteConfig = {};
-const AppNavigator = DrawerNavigator({}, RouteConfig);
+const Splash ={
+    screen: SplashScreen,
+    navigationOptions: {
+        header: null
+    }
+}
+
+const RouteConfig = {
+    initialRoute: 'Splash'
+};
+const AppNavigator = DrawerNavigator({
+    Splash: Splash
+}, RouteConfig);
 
 export default AppNavigator;
