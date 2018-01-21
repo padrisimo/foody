837b9d6fc6eda33134f53523ae57ef87804a53d5
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-21 23:01:55 +0100

    conect the app navigator, splash screen  and config store

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import { Provider } from 'mobx-react';
 import AppNavigator from './app/app.navigator';
+import stores from './app/stores';
 
 export default class App extends Component {
   render() {
     return (
-      <AppNavigator />
+      <Provider stores={stores} >
+        <AppNavigator />
+      </Provider>
     );
   }
 }
