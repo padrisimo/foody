deda0287470c98a198596a66be2c4c262ac37aed
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-27 17:47:37 +0100

    add custom theme

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { Provider } from 'mobx-react';
+import { StyleProvider } from 'native-base';
 import AppNavigator from './app/app.navigator';
 import stores from './app/stores';
+import getTheme from './native-base-theme/components';
+import custom from './native-base-theme/variables/custom';
 
 export default class App extends Component {
   render() {
     return (
       <Provider stores={stores} >
-        <AppNavigator />
+        <Styleprovider style={getTheme(custom)}>
+          <AppNavigator />
+        </Styleprovider>
       </Provider>
     );
   }
