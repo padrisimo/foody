a4809638f62bd719cc435b4fabb527ef7e4ebc58
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-27 20:30:45 +0100

    add login styles

--- app/components/login.component.js
+++ app/components/login.component.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import {  View, Text, } from 'react-native';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  
   render() {
     return null;
   }
--- app/screens/login.screen.js
+++ app/screens/login.screen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Content } from 'native-base';
-import { Image, View } from 'react-native';
+import { Image, View, StyleSheet, Dimensions } from 'react-native';
 import { inject } from 'mobx-react';
 import Login from '../components/login.component';
 
@@ -14,10 +14,10 @@ export default class LoginScreen extends Component {
         const {stores} = this.props;
         return (
             <Container>
-                <View>
+                <View style={styles.container}>
                     <Content scrollEnabled={false}>
-                        <Image source={stores.config.loginBG}>
-                            <View>
+                        <Image style={styles.loginBackground} source={stores.config.loginBG}>
+                            <View style={styles.loginForeground}>
                                 <Login {...this.props} />
                             </View>
                         </Image>
@@ -27,3 +27,26 @@ export default class LoginScreen extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        bottom: 0,
+        left:0,
+        right:0
+    },
+    loginBackground: {
+        flex: 1,
+        width: null,
+        height: null
+    },
+    loginForeground: {
+        flex: 1,
+        marginTop: Dimensions.get('window').height/1.75,
+        paddingTop: 20,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 90,
+        bottom: 0
+    }
+});
