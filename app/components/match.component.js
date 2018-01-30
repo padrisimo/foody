2113ca6611baa0436a3902fa9934982756b0fba6
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-30 15:37:26 +0100

    creating the match component

--- app/components/match.component.js
+++ app/components/match.component.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react';
+import {
+  Button,
+  Icon,
+  Spinner,
+  Card,
+  CardItem,
+  DeckSwiper
+} from 'native-base';
+import { View, Text, Image } from 'react-native';
+import { observer } from 'mobx-react/native';
+import { observable } from 'mobx';
+import { createAutoSubscriber, autoSubscriber } from 'firebase-nest';
 
-export default class Match extends Component {
+class Match extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      _autoSubscriberFetching: true,
+      _autoSbuscriberError: null
+    }
+  }
+  static getSubs(props, state) {
+    const { auth, matches } = props.stores;
+    return auth.authUser ? matches.subs() : [];
+  }
+  subscribeSubs(subs, props, state) {
+    const { matches } = props.stores;
+    return matches.subscribeSubsWithPromise(subs);
+  }
+  markViewed(match) {
+    this.props.stores.matches.markViewed(match[0]);
+  }
+  renderCard() {
+    const postObj = post ? post[1] : null;
+    if (postObj) {
+      let pic = { uri: postObj.url };
+      let text = postObj.text;
+
+      return (
+        <Card>
+          <CardItem cardBody>
+            {pic.uri != undefined && pic.uri != "" ? <Image source={pic} /> : null}
+          </CardItem>
+          <CardItem>
+            <Text>{text}</Text>
+          </CardItem>
+        </Card>
+      )
+    }
+    return null;
+  }
+  renderNoMoreCards(){
+    return (
+      <Card>
+        <CardItem cardBody>
+          Out of Matches
+        </CardItem>
+      </Card>
+    )
   }
   render() {
     return null;
   }
 };
+
+export default autoSubscriber(observer(Match))
