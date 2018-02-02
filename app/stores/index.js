8e75cfee96562b34cef081822d57a0e131d8c1e7
padrisimo <nopuedoparardemolar@outlook.com>
2018-02-02 20:06:38 +0100

    creating the post store

--- app/stores/index.js
+++ app/stores/index.js
@@ -1,9 +1,11 @@
 import ConfigStore from './config.store';
 import AuthStore from './auth.store';
 import MatchStore from './match.store';
+import PostStore from './post.store';
 
 const config = new ConfigStore()
 const auth = new AuthStore()
 const matches = new MatchStore()
+const post = new PostStore()
 
-export default { config, auth, matches };
+export default { config, auth, matches, post };
